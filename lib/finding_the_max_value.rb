e05ee088e14ee0c4255b1b066703a33e7ea1e735
Noah Eakin <noaheakin@gmail.com>
2020-08-10 17:14:50 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  array.sort.last
+  counter = 0
+  max_value = -1
+  while counter < array.length do
+    if array[counter] > max_value
+      max_value = array[counter]
+    end
+    counter += 1
+  end
+  max_value
 end
